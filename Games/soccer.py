2364e7c50a193ef13e2238bdefd5133ff1f21b2a
Huazhe Xu <huazhe_xu@Huazhes-MacBook-Pro.local>
2018-01-11 12:00:12 -0800

    clean up code, clarify soccer game

--- Games/soccer.py
+++ Games/soccer.py
@@ -2,93 +2,113 @@ import numpy as np
 
 
 class Soccer:
-    '''
-    Actions [0 : Left, 1 : Up, 2 : Right, 3 : Down, 4 : Stand]
-    '''
 
-    def __init__(self, h=4, w=5, pA=[3, 2], pB=[1, 1], goalPositions=[1, 2], ballOwner=0, drawProbability=0):
-        self.h = h
-        self.w = w
-        self.goalPositions = np.array(goalPositions)
-        self.positions = np.array([pA, pB])
-        self.initPositions = np.array([pA, pB])
-        self.ballOwner = ballOwner
-        self.drawProbability = drawProbability
+    # Actions [0 : Left, 1 : Up, 2 : Right, 3 : Down, 4 : Stand]
 
-    def restart(self, pA=None, pB=None, ballOwner=None):
-        if pA is not None:
-            self.initPositions[0] = pA
+    def __init__(self, height=4, width=5, pos_a=None, pos_b=None, ball_owner=0, draw_probability=0):
+        # [3, 2]
+        # [1, 1]
+        if pos_a is None:
+            pos_a = [2, 1]
+        if pos_b is None:
+            pos_b = [1, 3]
 
-        if pB is not None:
-            self.initPositions[1] = pB
+        self.height = height
+        self.width = width
+        self.positions = np.array([pos_a, pos_b])
+        self.initPositions = np.array([pos_a, pos_b])
+        self.ball_owner = ball_owner
+        self.draw_probability = draw_probability
+        self.step_count = 0
+        self.terminal = False
 
-        if ballOwner is None:
-            ballOwner = self.choosePlayer()
+    def reset(self, pos_a=None, pos_b=None, ball_owner=None):
+        if pos_a is not None:
+            self.initPositions[0] = pos_a
+        else:
+            self.initPositions[0] = [np.random.randint(self.height), 0]
+
+        if pos_b is not None:
+            self.initPositions[1] = pos_b
+        else:
+            self.initPositions[1] = [np.random.randint(self.height), self.width-1]
+
+        if ball_owner is None:
+            ball_owner = self.choose_player()
 
         self.positions = self.initPositions.copy()
-        self.ballOwner = ballOwner
+        self.ball_owner = ball_owner
+        self.step_count = 0
+        self.terminal = False
 
-    def play(self, actionA, actionB):
-        if np.random.rand() < self.drawProbability:
-            return -2
-        first = self.choosePlayer()
-        actions = [actionA, actionB]
-        m1 = self.move(first, actions[first])
-        if (m1 >= 0):
-            return m1
-        return self.move(1 - first, actions[1 - first])
+    def step(self, action_a, action_b):
+        status = np.NaN
+        if self.step_count >= 10:
+            self.terminal = True
+            return status, self.terminal
+        first = self.choose_player()
+        second = 1 - first
+        action_in_play = [action_a, action_b]
+        m1 = self.move(first, action_in_play[first])
+        if m1 == 1:
+            print("Goal!! Need to reset.")
+            return m1, first
+        return self.move(second, action_in_play[1 - first]), second
 
-    def move(self, player, action):
+    def move(self, player, action_move):
         opponent = 1 - player
-        newPosition = self.positions[player] + self.actionToMove(action)
+        new_position = self.positions[player] + self.action_to_move(action_move)
 
         # If it's opponent position
-        if (newPosition == self.positions[opponent]).all():
-            self.ballOwner = opponent
+        if (new_position == self.positions[opponent]).all():
+            self.ball_owner = opponent
         # If it's the goal
-        elif self.ballOwner is player and self.isInGoal(*newPosition) >= 0:
-            return 1 - self.isInGoal(*newPosition)
+        elif self.ball_owner is player and self.is_ingoal(new_position[0], new_position[1], player) :
+            return 1
         # If it's in board
-        elif self.isInBoard(*newPosition):
-            self.positions[player] = newPosition
-        return -1
+        elif self.is_inboard(*new_position):  # * here is to use component of new_position as argument
+            self.positions[player] = new_position
+        # invalid action -> nothing happens. return 0 means no goal happens.
+        return 0
 
-    def actionToMove(self, action):
+    def action_to_move(self, action):
+        # Actions [0 : Left, 1 : Up, 2 : Right, 3 : Down, 4 : Stand]
         switcher = {
-            0: [-1, 0],
-            1: [0, 1],
-            2: [1, 0],
-            3: [0, -1],
-            4: [0, 0],
+            0: [0, -1],
+            1: [-1, 0],
+            2: [0,  1],
+            3: [1,  0],
+            4: [0,  0],
         }
         return switcher.get(action)
 
-    def isInGoal(self, x, y):
-        g1, g2 = self.goalPositions
-        if (g1 <= y <= g2):
-            if x == -1:
-                return 1
-            elif x == self.w:
-                return 0
-        return -1
+    def is_ingoal(self, h, w, player):
+        assert(player == 0 or player == 1)
+        if player == 0:
+            if (h == 1 or h == 2) and w == 5:
+                return True
+        elif player == 1:
+            if (h == 1 or h == 2) and w == -1:
+                return True
+        return False
 
-    def isInBoard(self, x, y):
-        return (0 <= x < self.w and 0 <= y < self.h)
+    def is_inboard(self, h, w):
+        return 0 <= h < self.height and 0 <= w < self.width
 
-    def choosePlayer(self):
+    def choose_player(self):
         return np.random.randint(0, 2)
 
-    def draw(self, positions=None, ballOwner=None):
+    def render(self, positions=None, ball_owner=None):
         positions = self.positions if positions is None else np.array(positions)
-        ballOwner = self.ballOwner if ballOwner is None else ballOwner
-
+        ball_owner = self.ball_owner if ball_owner is None else ball_owner
+        #import pdb; pdb.set_trace()
         board = ''
-        for y in range(self.h)[::-1]:
-            for x in range(self.w):
-                if ([x, y] == positions[0]).all():
-                    board += 'A' if ballOwner is 0 else 'a'
-                elif ([x, y] == positions[1]).all():
-                    board += 'B' if ballOwner is 1 else 'b'
+        for h in range(self.height):
+            for w in range(self.width):
+                if ([h, w] == positions[0]).all():
+                    board += 'A' if ball_owner is 0 else 'a'
+                elif ([h, w] == positions[1]).all():
+                    board += 'B' if ball_owner is 1 else 'b'
                 else:
                     board += '-'
             board += '\n'
@@ -97,10 +117,11 @@ class Soccer:
 
 
 if __name__ == '__main__':
-    s = soccer()
-    s.draw()
-    actions = [[0, 1], [0, 4], [1, 3], [1, 0], [1, 0]]
-    actions = [[0, 4], [0, 4], [0, 4], [1, 4], [0, 4]]
+    s = Soccer()
+    s.render()
+    actions = [[2, 1], [2, 4], [2, 3], [2, 0], [2, 0]]
+    # actions = [[0, 4], [0, 4], [0, 4], [1, 4], [0, 4]]
     for action in actions:
-        print s.play(*action)
-        s.draw()
+        s.step(*action)
+        print(action)
+        s.render()
